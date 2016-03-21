8b493e11ad32f046538c660468c984578f6b8cf4
ofbennett <ofbennett@gmail.com>
2016-03-21 13:28:27 +0000

    Changes to shell script

--- legion_build_run_fetch.sh
+++ legion_build_run_fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 pip install fabric
-fab legion.cold
-fab legion.sub
+fab legion.cold:branch=OpenMP
+fab legion.sub:processes=1
 fab legion.wait
-fab legion.fetch_all
+fab legion.fetch_time
 fab legion.cleanup_all
 python make_video.py results/latest_results/legion.rc.ucl.ac.uk/output.txt
