3137351b87fbdbf47c0929b3c3f286a497fcef26
ofbennett <ofbennett@gmail.com>
2016-03-25 16:42:45 +0000

    Created MPI shell script for creating performance graph

--- make_graph.py
+++ make_graph.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 from os.path import join, dirname, abspath
 from glob import glob
 
-proc_nums = [1,2,3,4,6,8,10,12]
+proc_nums = [1,2,4,6,8,10,12]
 
 fname_list = []
 for i in proc_nums:
-    fname = glob('./results/{}_processes/legion.rc.ucl.ac.uk/*'.format(i))[0]
+    fname = './results/{}_processes/legion.rc.ucl.ac.uk/time.txt'
     fname_list.append(fname)
 
 time_list = []
@@ -18,8 +18,8 @@ for files in fname_list:
 plt.plot(proc_nums,time_list,marker='o')
 ymin, ymax = plt.ylim()
 plt.ylim(0,ymax)
-plt.title('Run Time vs Process Number')
-plt.xlabel('Process number')
+plt.title('Run Time vs MPI Process Number')
+plt.xlabel('MPI Process number')
 plt.ylabel('Run time / sec')
 
 plt.show()
