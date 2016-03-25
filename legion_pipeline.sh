aeb73acb0357f6291b58a45256931afa97476b08
ofbennett <ofbennett@gmail.com>
2016-03-25 18:41:21 +0000

    Change to deployment shell script

--- legion_pipeline.sh
+++ legion_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 pip install fabric
 fab legion.cold:branch=MPI
-fab legion.sub:processes=4,config=config.yml
+fab legion.sub:processes=9,config=config.yml
 fab legion.wait
 fab legion.fetch_all
 fab legion.cleanup_all
